98bcda0581606ab0861a4b60b19531b940485f51
mbish08 <mdrew8@gmail.com>
2020-12-05 18:38:35 -0800

    update migrations for tables

--- db/migrate/20201206013206_create_coffees.rb
+++ db/migrate/20201206013206_create_coffees.rb
@@ -1,10 +1,11 @@
-class CreateCoffees < ActiveRecord::Migration
+class CreateCoffees < ActiveRecord::Migration[5.2]
   def change
     create_table :coffees do |t|
       t.string :brand
       t.string :flavor
       t.string :roast
       t.float :rating
+      t.integer :user_id
     end 
   end
 end
