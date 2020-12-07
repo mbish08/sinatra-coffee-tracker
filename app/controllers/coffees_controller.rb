0a9b88c0a7d26e94148f6f43d9c698734fda4c0c
mbish08 <mdrew8@gmail.com>
2020-12-06 17:10:24 -0800

    building out coffees routes

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -10,29 +10,37 @@ class CoffeesController < ApplicationController
         erb :'/coffees/new'
     end 
 
-    # post '/coffees' do 
-    #     #submit the form and create a coffee and redirect
-    #     erb :coffees
-    # end 
+    post '/coffees' do 
+        #submit the form and create a coffee and redirect
+        @coffees = Coffee.all
+        redirect to :'coffees/index'
+    end 
 
     # get '/coffees/:id' do
     #     #show one coffee
-
+    #     ##NEED TO CODE IN AN IF/ELSE FOR IF FOUND DO X, ELSE DO Y - IF ID ISN'T FOUND IT WILL RETURN 'NIL'
+    #     @coffee = Coffee.find_by(id: params[:id])
+    #     erb :'movies/show'
     # end 
 
     # get '/coffees/:id/edit' do
     #     #get the form to edit one coffee
-
+    #     @coffee = Coffee.find_by_id(params[:id])
+    #     erb :'coffees/edit'
     # end 
 
     # patch '/coffees/:id' do 
     #     #update/modify the existing coffee in the database
-
+    #     movie = Movie.find_by_id(params[:id])
+    #     movie.update(params)
+    #     redirect to :'movies/index'
     # end 
 
-    # delete '/coffees/:id' do
-    #     #delete a single coffee 
-
-    # end 
+    delete '/coffees/:id' do
+        #delete a single coffee 
+        movie = Movie.find_by_id(params[:id])
+        movie.destroy
+        redirect to :'moveis/index'
+    end 
 
 end 
