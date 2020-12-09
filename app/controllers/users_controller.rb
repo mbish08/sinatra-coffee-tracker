3143116115fd7022904e196e96572c7838c11255
mbish08 <mdrew8@gmail.com>
2020-12-08 17:47:43 -0800

    draft of sign up form and update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
 
+
+    get '/signup' do
+        #get form to sign up
+        erb :'users/signup'
+    end
+
+    post '/signup' do
+        
+    end 
+     
 end 
