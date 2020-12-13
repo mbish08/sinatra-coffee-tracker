d7ab22e35bbbf7a93ba1f45fd3c4587747762820
mbish08 <mdrew8@gmail.com>
2020-12-13 10:38:55 -0800

    fixed home page link and updated user class to add pw validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base 
     has_secure_password
+    validates :password, length: {minimum: 6}, allow_blank: false
     validates :username, presence: true
     validates :username, uniqueness: true
     validates :email, presence: true
